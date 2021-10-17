a92dcc53ceea214900e8f55249e9a26926e53ff0
dangolbeeker <scottb305@outlook.com>
2021-10-17 15:46:02 -0500

    db backup method with gcp cloud storage, path and aws s3 paths

--- README.md
+++ README.md
@@ -1,2 +1,13 @@
-# fastify-pg-backup
+
+#Fastify-Production-Postgres
 Plugin for Fastify Automated Database Exports in Postgresdb 
+
+
+This plugin has all the features of  fastify-postgres with a few additions for production optimization.
+
+1)Schedule Automated Database Exports path or aws s3 bucket
+2)Built in Native Support 
+
+
+TODO
+
---
+++ index.d.ts
@@ -0,0 +1,15 @@
+// const { fastifypostgres } = require('fastify-postgres');
+// // const { pgnative } = require('pg-native');
+// // const { fastifySchedulePlugin } = require('fastify-schedule')
+// // const { execute } = require('@getvim/execute');
+// // const compress = require('gzipme');
+// // const axios = require('axios');
+// // const FormData = require('form-data');
+// // const fs = require('fs');
+// // const cron = require('node-cron');
+// // const fp = require('fastify-plugin');
+
+
+// // function fastifyProdPostgres (fastify, opts, next) {
+// //     const onConnect 
+// // }
